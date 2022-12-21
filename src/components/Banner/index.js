a12a3ed5cb7d9ac09791c6b2f245dc34998be6f8
Tamirys Virgulino Ribeiro Prado <tamirysvirgulino@MacBook-Pro-de-Tamirys.local>
2022-12-21 11:51:03 -0300

    SPA com react-router-dom

---
+++ src/components/Banner/index.js
@@ -0,0 +1,30 @@
+import styles from './Banner.module.css';
+import circuloColorido from 'assets/circulo_colorido.png'
+import minhaFoto from 'assets/minha_foto.png'
+
+export default function Banner() {
+    return (
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Olá, mundo!
+                </h1>
+                <p className={styles.paragrafo}>
+                    Boas vindas ao meu espaço pessoal.
+                </p>
+            </div>
+            <div className={styles.imagens}>
+                <img 
+                    className={styles.circuloColorido}
+                    src={circuloColorido}
+                    aria-hidden={true}
+                />
+                <img 
+                    className={styles.minhaFoto}
+                    src={minhaFoto}
+                    alt="Foto"
+                />
+            </div>
+        </div>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Menu from "./components/Menu";
 import Inicio from "./paginas/Inicio";
 import Sobre from "./paginas/Sobre";
 
-function App() {
+function AppRoutes() {
   return (
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Inicio/>} />
         <Route path="/sobre" element={<Sobre/>}/>
@@ -14,4 +16,4 @@ function App() {
   )
 }
 
-export default App;
+export default AppRoutes;
